86191deeea1e401d8df80030b9279887c3a4ec2f
Noah Kim <noahbkim@gmail.com>
2014-04-30 21:33:23 -0400

    Some stuff

--- chat.py
+++ chat.py
@@ -4,10 +4,32 @@
 # For Africa!
 
 # Import
+import tkinter
+import tkinter.scrolledtext
+import tkinter.messagebox
+
 import socketserver
 import threading
-import tkinter
+
 import pickle
 import queue
 import logging
 import time
+
+# Debug
+from logging import FATAL, CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+logging.basicConfig(format="[%(asctime)s] %(levelname)-8s %(message)s",
+                    datefmt="%H:%M:%S", level=NOTSET)
+
+# Constant
+RECV = 1024
+
+# Function
+def message(**values):
+    return values
+
+def hide(ip):
+    return ".".join(ip.split(".")[:2] + ["xxx",] * 2)
+
+# Class
+# NOPE
